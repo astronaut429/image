37b2db5e2ef812b4db52d428cf9db33f75b844ac
Daniel Nurtai <daniel.nurtai1@gmail.com>
2023-03-11 12:24:45 -0500

    Change: Added code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hey!!!");
+const sharp = require("sharp");
+
+async function resizeImage() {
+  try {
+    await sharp("g.jpg")
+      .resize({
+        width: 50,
+        height: 30,
+      })
+      .toFile("g_resize.jpg");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+resizeImage();
